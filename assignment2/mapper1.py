49b40c149bf75812f7c2d163b6024ad64da5d3a5
Sanket <sanketdonty@gmail.com>
2020-09-23 00:56:30 +0530

    Added an alternative reducer implementation

--- assignment2/mapper1.py
+++ assignment2/mapper1.py
@@ -7,4 +7,6 @@ import sys
 for line in sys.stdin:
     line=line.strip()
     words=line.split("\t")
+    # words[0] = from_node_id
+    # words[1] = to_node_id
     print('{0}\t{1}'.format(words[0],words[1]))
