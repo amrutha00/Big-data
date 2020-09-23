0c6e8a0600a3fee6de9cd69c76995fc6db3310d4
amrutha00 <51371154+amrutha00@users.noreply.github.com>
2020-09-23 15:48:06 +0530

    Add files via upload

---
+++ assignment2/reducer1 .py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import sys
+
+D={}
+
+file=open("v.txt","w+")
+
+
+
+for line in sys.stdin:
+    line=line.strip()
+    try:
+        from_node,to_node=line.split("\t")
+    except:
+        from_node=line.strip("\t")
+        to_node=-1
+    """    
+    if from_node.isnumeric():
+        from_node=int(from_node)
+    if to_node.isnumeric():
+        to_node=int(to_node)    
+    """
+    if from_node not in D:
+        if to_node==-1:
+            D[from_node]=[]
+        else:
+            D[from_node]=[to_node]
+    else:
+        D[from_node].append(to_node)
+    
+for key,value in D.items():
+    value.sort()
+    print('{0}\t{1}'.format(key,value))
+    file.write('{0}\t{1}'.format(key,1)+'\n')
+
+
+     
+
+
+file.close()
