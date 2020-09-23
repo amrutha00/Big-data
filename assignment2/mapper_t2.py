d2bcc2ea3c0472aaece112394b03f38d503ad8d8
Sanket <sanketdonty@gmail.com>
2020-09-23 17:28:23 +0530

    Added mapper for task 2

---
+++ assignment2/mapper_t2.py
@@ -0,0 +1,23 @@
+import sys
+import ast
+
+path_to_v = sys.argv[1]
+f = open(path_to_v, "r")
+
+pgrank = dict()
+
+for line in f:
+	line = line.strip()
+	node, pagerank = line.split(", ")
+	pgrank[node] = float(pagerank)
+
+
+for line in sys.stdin:
+	line = line.strip()
+	from_node, to_nodes = line.split("\t")
+	to_nodes = ast.literal_eval(to_nodes)
+	to_nodes = [n.strip() for n in to_nodes]
+	n = len(to_nodes)
+	for i in to_nodes:
+		print(i, round(pgrank[from_node]/float(n), 5), sep="\t")
+	
