6f68ac360111982145167073841c37c844728bd8
Khushei Meghana Meda <43701562+khusheimeda@users.noreply.github.com>
2020-10-03 13:43:52 +0530

    Latest submission by Khushei, removed format

--- assignment2/BD_0210_0416_1879_2057_mapper_t1.py
+++ assignment2/BD_0210_0416_1879_2057_mapper_t1.py
@@ -12,7 +12,6 @@ for line in sys.stdin:
         
     words=line.split("\t")
       
-    # words[0] = from_node_id
-    # words[1] = to_node_id
     if len(words) == 2:
-        print('{0}\t{1}'.format(words[0],words[1]))
+        #print('{0}\t{1}'.format(words[0],words[1]))
+        print(words[0] + '\t' + words[1])
