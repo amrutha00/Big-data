8b96c79cfa9a4d52aec43555a697cbb465b1fbb6
Sanket <sanketdonty@gmail.com>
2020-11-07 20:51:13 +0530

    Final files

--- assignment3/task1.py
+++ assignment3/task1.py
@@ -4,30 +4,23 @@ from pyspark.sql import functions as F
 
 import sys
 
-# SparkContext
 spark = SparkSession \
 	.builder \
 	.appName("Task 1") \
 	.getOrCreate()
 
-# Checking for the number of arguments
 if (len(sys.argv) != 4):
 	print("Wrong number of arguments")
 	sys.exit(-1)
 
-# First command line argument = the word value
 wordValue = sys.argv[1]
-df = spark.read.load(sys.argv[2], format="csv", inferSchema="true", header="true")
+df = spark.read.load(sys.argv[3], format="csv", inferSchema="true", header="true")
 
-# wordRows contains all the rows that has the given word
-wordRows = df.filter(df['word'] == wordValue)
+wordRows = df.filter(df['word'] == wordValue).cache()
 
-# When booTrue is true, its sum and count value don't have to be computed
-# Similarly for booFalse
 booTrue = False
 booFalse = False
 
-# When the word doesn't exist in the dataset, update sum and count values for both true and false
 if (len(wordRows.head(1)) == 0):
 	truecount = 1
 	truesum = 0.00000
@@ -35,16 +28,11 @@ if (len(wordRows.head(1)) == 0):
 	falsecount = 1
 	booTrue = True
 	booFalse = True
-
-
 a = []
-
-# When the word does exist in the dataset
 if (not (booTrue and booFalse)):
 	counts = wordRows.groupBy("recognized").count()
 	a = counts.collect()
 
-# If all the rows containing the word have the same recognized value (either true or false), update accordingly
 if (len(a) == 1):
 	if (a[0][0] == True):
 		falsesum = 0.00000
@@ -56,13 +44,9 @@ if (len(a) == 1):
 		truesum = 0.00000
 		booTrue = True
 		falsecount = a[0][1]
-
-# If all the rows containing the word have both true and false values in dataset under recognized column
 elif (len(a) == 2):
 	truecount = a[0][1]
 	falsecount = a[1][1]
-
-# Else, the word doesn't exist, and we've already taken care of this case (line 42)
 
 if (not booTrue):
 	truerows = wordRows.filter(df['recognized'] == "true")
--- assignment3/task2.py
+++ assignment3/task2.py
@@ -17,32 +17,25 @@ wordValue = sys.argv[1]
 k = int(sys.argv[2])
 df_shapestat= spark.read.load(sys.argv[4], format="csv", inferSchema="true", header="true")
 df_shape= spark.read.load(sys.argv[3], format="csv", inferSchema="true", header="true")
-df_shapestat.printSchema()
-df_shape.printSchema()
+#df_shapestat.printSchema()
+#df_shape.printSchema()
 
 shapestat=df_shapestat.select("word","recognized","Total_Strokes", "key_id")
-# shape=df_shape.drop("key_id")
+
 
 wordRows = shapestat.filter(df_shapestat['word'] == wordValue)
-# wordRows = wordRow.withColumnRenamed("word","key")
+
 if(len(wordRows.head(1)) == 0):
-	pass
+	print("0");
 
 
 else:
-	# condition=[shape.word== wordRows.key]
-	# df_joined=shape.join(wordRows,condition,'inner')
-	df_joined = df_shape.join(wordRows, 'key_id', 'inner')
-	#df_joined.printSchema()
-	#df=df_joined.select(df_joined.key,df_joined.countrycode,df_joined.recognized,df_joined.Total_Strokes)
-	df=df_joined.drop("key")
-	df1=df.filter(df_joined["recognized"]=="FALSE")
-	#print(df1)
+	df = df_shape.join(wordRows, 'key_id', 'inner')
+	df1=df.filter(df["recognized"]=="FALSE")
 	df2=df1.filter(df1["Total_Strokes"]<k)
-	#print(df2)
 	df3=df2.groupBy("countrycode").count().orderBy("countrycode",ascending=True).collect()
-	print(df3)
+	#print(df3)
 
 	for i in range(0,len(df3)):
-		print(df3[i][0],(df3[i][1]),sep=",")
-
+		if(df3[i][1]!=0):
+			print(df3[i][0],(df3[i][1]),sep=",")
