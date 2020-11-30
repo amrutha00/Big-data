b350f6b967cf5b2f129582e4bce22cd767dd29f7
Sanket Donty <53693680+firefire-iceice@users.noreply.github.com>
2020-11-30 12:41:59 +0530

    Update start-all.sh

--- project/start-all.sh
+++ project/start-all.sh
@@ -1,5 +1,6 @@
 python3 worker1.py 4000 1 &
 python3 worker2.py 4001 2 &
 python3 worker3.py 4002 3 &
-python3 master.py config.json random &
+python3 worker4.py 4003 4 &
+python3 master.py config.json 'least' &
 
